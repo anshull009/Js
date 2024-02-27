02b4c28e7bbf025f55863df4f5ff3237d5fcfcde
anshull009 <161447925+anshull009@users.noreply.github.com>
2024-02-27 11:04:48 +0000

    Datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer Version
+// alert( 3 + 3) // we are using nodejs, not browser
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof true)
---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+const accountId =12345
+let accointEmail="Abcd@12345"
+var accountPassword = "Password"
+accountCity="Imdore"
+
+// accountId =2 // not allowed
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.log(accountId);
+console.table([accountId , accointEmail ,accountPassword , accountCity])
