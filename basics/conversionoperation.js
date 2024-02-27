6aae875555578f5adfc33466a98dd13f61bcd082
anshull009 <161447925+anshull009@users.noreply.github.com>
2024-02-27 11:48:09 +0000

    conversion of data type

---
+++ basics/conversionoperation.js
@@ -0,0 +1,27 @@
+let score = "hitesh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+
+let isLoggedIn = 1
+
+let boooleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(boooleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let sum = 33
+let string = String(sum)
+console.log(string);
