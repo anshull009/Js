a199231b8faf4308bcdc308b5c7998f71ec30da1
anshull009 <161447925+anshull009@users.noreply.github.com>
2024-05-05 09:06:20 +0000

    compair

---
+++ basics/compair.js
@@ -0,0 +1,11 @@
+// console.log("2">1)
+// console.log("02"<1)
+// console.log(null<=0)
+// console.log(null==0)
+// console.log(null>=0)
+// console.log(null>0)
+
+//console.log(undefined<=0)
+
+console.log("2" === 2)
+// === is used for strict cheack it not only check the value but also check the data type too
