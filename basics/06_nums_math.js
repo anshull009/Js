4ccb7a35a267be718ec5c92873e1878afcb92e7a
anshull009 <161447925+anshull009@users.noreply.github.com>
2024-05-05 12:09:45 +0000

    Math

---
+++ basics/06_nums_math.js
@@ -0,0 +1,25 @@
+const score = 100
+const balance = new Number(100)
+//console.log(balance)
+
+//console.log(balance.toString())
+//console.log(balance.toFixed(1))
+
+const next = 423.55578
+//console.log(next.toPrecision(3));
+
+const a = 10000000000
+//console.log(a.toLocaleString(`en-IN`));
+
+// ****************Math***************
+
+// console.log(Math)
+// console.log(Math.abs (-44))
+// console.log(Math.round (4.6))
+// console.log(Math.ceil (4.2))
+// console.log(Math.floor (4.58))
+// console.log((Math.random()*6)+1);
+const min = 1
+const max = 6
+
+console.log(Math.floor(Math.random()*(max-min + 1 ))+min)
