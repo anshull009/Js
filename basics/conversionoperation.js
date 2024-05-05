cbdd574441fb17e8ddef89086a97bf6f57d59311
anshull009 <161447925+anshull009@users.noreply.github.com>
2024-05-05 08:46:15 +0000

    Conversion operators

--- basics/conversionoperation.js
+++ basics/conversionoperation.js
@@ -16,7 +16,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = 1
 
 let boooleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(boooleanIsLoggedIn)
+//console.log(boooleanIsLoggedIn)
 
 // 1 => true; 0 => false
 // "" => false
@@ -24,4 +24,25 @@ console.log(boooleanIsLoggedIn)
 
 let sum = 33
 let string = String(sum)
-console.log(string);
+//console.log(string);
+let a = 'a'
+//console.log(typeof a)
+
+//*****************************operations ********************************//
+
+let value = 3 
+let negative  = -value
+//console.log(negative)
+//console.log(2**3)
+//console.log(2*3)
+//console.log(2/3)
+//console.log(2%3)
+
+let str1 ="hello"
+let str2 = " anshul"
+// console.log(str1+str2)
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + "2" + 1) 
+// console.log("1" + 2 + 1) 
+// console.log(1 + 1+"2")
